c174c23a401c2b938bca2c6b2a964429944eaafd
Runrioter <runrioter@qq.com>
2018-10-27 01:00:24 +0800

    fix: Make footer better

--- assets/js/scale.fix.js
+++ assets/js/scale.fix.js
@@ -1,17 +1,21 @@
-var metas = document.getElementsByTagName('meta');
-var i;
-if (navigator.userAgent.match(/iPhone/i)) {
-  for (i=0; i<metas.length; i++) {
-    if (metas[i].name == "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+(function() {
+  var vps = document.getElementsByName('viewport');
+  if (vps.length && navigator.userAgent.match(/iPhone/i)) {
+    vps[0].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+    document.addEventListener("gesturestart", function() {
+      vps[0].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
+    }, false);
+  }
+  var contentHeight = document.body.scrollHeight;
+  var windowHeight = window.innerHeight;
+  if (contentHeight < windowHeight) {
+    var siteFooters = document.getElementsByClassName('site-footer');
+    if (siteFooters.length) {
+      var sf = siteFooters[0];
+      sf.style.position = 'absolute';
+      sf.style.bottom = 0;
+      sf.style.left = 0;
+      sf.style.right = 0;
     }
   }
-  document.addEventListener("gesturestart", gestureStart, false);
-}
-function gestureStart() {
-  for (i=0; i<metas.length; i++) {
-    if (metas[i].name == "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
-    }
-  }
-}
+})();
